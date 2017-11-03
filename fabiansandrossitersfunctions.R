5163e8549154392deb6a051953380954bab202a8
fernstgruber <fabianernstgruber@hotmail.com>
2017-11-03 14:21:35 +0100

    Update fabiansandrossitersfunctions.R

--- fabiansandrossitersfunctions.R
+++ fabiansandrossitersfunctions.R
@@ -774,7 +774,7 @@ increaseacc <- function(modeldata,dependent,pset){
   print(paste("OBB error with all predictors of ",paramsetnames[pset], "is ",fullmodel$err.rate[nrow(fullmodel$err.rate),1]))
   importance <- as.data.frame(fullmodel$importance)
   importance <- importance[order(importance$MeanDecreaseAccuracy,decreasing = T),]
-  print(kable(importance[1:10,]))
+  print(kable(as.data.frame(importance[1:10,])))
   return(importance[1:10,])
    cat('\n')
 }
