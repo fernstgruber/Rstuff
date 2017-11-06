d2f177ffceeda99b9bb939dda438cc567081f1fd
fernstgruber <fabianernstgruber@hotmail.com>
2017-11-06 15:37:29 +0100

    Update fabiansandrossitersfunctions.R

--- fabiansandrossitersfunctions.R
+++ fabiansandrossitersfunctions.R
@@ -908,6 +908,7 @@ Modus <- function(x) {
       predictor_df[[kname]]<- result_df$metric
       allchosen <-c(allchosen,as.character(result_df$metric))
     }
+    cat("\n")
     print(paste("Prediction error at end is: ",vcpreds[,"meanprederror"]))
     print(predictor_df)
     #as.data.frame(table(allchosen))[order(as.data.frame(table(allchosen))$Freq,decreasing=TRUE),]
