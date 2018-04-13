880aca9b18e8febe085dfe280f6e05d8c3e18bd8
fernstgruber <fabianernstgruber@hotmail.com>
2018-04-13 10:03:58 +0200

    plotfunctions for geoms and terrain parameters

--- fabiansandrossitersfunctions.R
+++ fabiansandrossitersfunctions.R
@@ -1168,3 +1168,46 @@ schluff_oe <- function(portions,breaks){
   return(textur)
 }
 
+geombarplot <- function(geomcol,thema,einheit,data,...){
+  code= c(1:10)
+  geom_names=c("flat","peak","ridge","shoulder","spur","slope","hollow","footslope","valley","pit")
+  n<-geom_names
+  mycols <- c("#e2e0e1","#000000","#c0262a","#d78321","#e7cb3c","#fcea0d","#bfd71c","#82b16b","#3e3ab8","#000000")
+  geomorphons <- as.data.frame(cbind(code,geom_names,mycols))
+  if(length(na.omit(data[data[[thema]] == einheit,geomcol]))<1){
+    L=data.frame("Var1" = 1:10,"Freq" = 0)
+  } else{
+    L<-as.data.frame(table(na.omit(data[data[[thema]] == einheit,geomcol])))
+  }
+  names(L) <- c("code","freq")
+  L$freqrel <- L$freq/sum(L$freq) * 100
+  L$code <- factor(L$code,levels=1:10)
+  L <- merge(L,geomorphons,all.y=TRUE)
+  L <- L[order(L$code),]
+  main=paste(einheit,geomcol,sep="  -  ")
+  par(mar=c(0.5,3,0.5,0))
+  barplot(L$freqrel,col=mycols,cex.main=0.5,las=2,ylab="%",ylim=c(0,100),cex.axis = 0.6,cex.lab=0.7, mgp=c(2, 1, 0),main=paste(rdata,sep=""),cex.main=0.8)
+}
+parameterboxplots <- function(origmodeldata,preddata, dependent,parameter){
+  ylim=c(summary(origmodeldata[[parameter]])[1],summary(origmodeldata[[parameter]])[6])
+  ylim2=c(summary(preddata[[parameter]])[1],summary(preddata[[parameter]])[6])
+  
+    boxplot(data=origmodeldata[c(as.character(parameter),dependent)],
+          as.formula(paste(as.character(parameter),"~ ", dependent,sep=" ")),
+          outline=T,
+          main=paste(as.character(parameter),"\n for ",rdata,sep=""),
+          ylim=ylim,
+          varwidth=T,
+          border="green",
+          cex.main=0.8)
+  boxplot(data=preddata[c(as.character(parameter),"preds")],
+          as.formula(paste(as.character(parameter),"~ ", "preds",sep=" ")),
+          outline=F,
+          ylim=ylim,
+          varwidth=T,
+          add=T,
+          border="red")
+  for (i in 1:5){
+    text(labels=paste('n = ',nrow(origmodeldata[origmodeldata[[dependent]] == i,]),sep=""),x=i,y=ylim[1]+0.005)
+  }
+}
